d4e80e3c15960720aafb40938ac0f6b593bd9d2c
Villars Gimm <villars.gimm@xamarin.com>
2014-02-06 16:41:00 +0100

    Add permission control to: Location Contacts Calendar Reminder Photos Bluetooth

--- Permissions/ViewController.h
+++ Permissions/ViewController.h
@@ -5,9 +5,12 @@
 //  Created by Villars Gimm on 06/02/14.
 //  Copyright (c) 2014 Villars Gimm. All rights reserved.
 //
+@import CoreBluetooth;
 
 #import <UIKit/UIKit.h>
+#import <CoreLocation/CoreLocation.h>
 
-@interface ViewController : UIViewController
+
+@interface ViewController : UIViewController <CLLocationManagerDelegate, UIImagePickerControllerDelegate, CBCentralManagerDelegate>
 
 @end
--- Permissions/ViewController.m
+++ Permissions/ViewController.m
@@ -6,13 +6,28 @@
 //  Copyright (c) 2014 Villars Gimm. All rights reserved.
 //
 
+@import AddressBook;
+@import EventKit;
+@import AVFoundation;
+
 #import "ViewController.h"
 
 @interface ViewController ()
 
+//iOS permissions
+@property (nonatomic, retain)CLLocationManager *locationManager;
+@property (nonatomic, retain)EKEventStore *eventStore;
+@property (nonatomic, retain)UIImagePickerController *picker;
+@property (nonatomic, strong)CBCentralManager *cbManager;
+
+- (ABAddressBookRef)addressBook;
+- (void)setAddressBook:(ABAddressBookRef)newAddressBook;
+
 @end
 
-@implementation ViewController
+@implementation ViewController{
+    ABAddressBookRef _addressBook;
+}
 
 - (void)viewDidLoad
 {
@@ -24,6 +39,127 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)locationManager:(CLLocationManager *)manager didChangeAuthorizationStatus:(CLAuthorizationStatus)status{
+    NSLog(@"CL Delegate");
+}
+
+void handleAddressBookChange(ABAddressBookRef addressBook, CFDictionaryRef info, void *context) {
+    /*
+     Do something with changed addres book data...
+     */
+}
+
+- (IBAction)buttonClick:(id)sender {
+    UIButton *button = (UIButton *)sender;
+    NSString *buttonTitle = button.currentTitle;
+    
+    if ([buttonTitle isEqualToString:@"Location Services"]) {
+        
+        NSLog(@"Location Services clicked");
+        
+        // CLLocationManager *locationManager;
+        self.locationManager = [[CLLocationManager alloc] init];
+        self.locationManager.delegate = self;
+        
+        [self.locationManager startUpdatingLocation];
+    }
+    else if ([buttonTitle isEqualToString:@"Contacts"]){
+        ABAddressBookRef addressBook = ABAddressBookCreateWithOptions(NULL, NULL);
+        
+        if(addressBook) {
+            self.addressBook = CFAutorelease(addressBook);
+            /*
+             Register for a callback if the addressbook data changes this is important to be notified of new data when the user grants access to the contacts. the application should also be able to handle a nil object being returned as well if the user denies access to the address book.
+             */
+            ABAddressBookRegisterExternalChangeCallback(self.addressBook, handleAddressBookChange, (__bridge void *)(self));
+            
+            /*
+             When the application requests to receive address book data that is when the user is presented with a consent dialog.
+             */
+            ABAddressBookRequestAccessWithCompletion(self.addressBook, ^(bool granted, CFErrorRef error) {
+                /*dispatch_async(dispatch_get_main_queue(), ^{
+                 [self alertViewWithDataClass:Contacts status:(granted) ? NSLocalizedString(@"GRANTED", @"") : NSLocalizedString(@"DENIED", @"")];
+                 });*/
+            });
+        }
+    }
+    else if ([buttonTitle isEqualToString:@"Calendars"]){
+        
+        NSLog(@"Calendar is called");
+        
+        self.eventStore = [[EKEventStore alloc] init];
+        
+        [self.eventStore requestAccessToEntityType:EKEntityTypeEvent completion:^(BOOL granted, NSError *error) {}];
+        
+    }
+    else if ([buttonTitle isEqualToString:@"Reminders"]){
+        
+        self.eventStore = [[EKEventStore alloc] init];
+        
+        [self.eventStore requestAccessToEntityType:EKEntityTypeReminder completion:^(BOOL granted, NSError *error) {}];
+    }
+    else if ([buttonTitle isEqualToString:@"Photos"]){
+        NSLog(@"Photos requested");
+        UIImagePickerController *picker = [[UIImagePickerController alloc]init];
+        picker.delegate = self;
+        //[picker setDelegate:self];
+        
+        [self presentViewController:picker animated:YES completion:nil];
+    }
+    else if ([buttonTitle isEqualToString:@"Bluetooth Sharing"]){
+        NSLog(@"Bluetooth Sharing");
+        if(!self.cbManager) {
+            self.cbManager = [[CBCentralManager alloc] initWithDelegate:self queue:nil];
+        }
+        
+        /*
+         When the application requests to start scanning for bluetooth devices that is when the user is presented with a consent dialog.
+         */
+        [self.cbManager scanForPeripheralsWithServices:nil options:nil];
+    }
+    else if ([buttonTitle isEqualToString:@"Microphone"]){
+        NSLog(@"Microphone");
+        AVAudioSession *session = [[AVAudioSession alloc] init];
+        [session requestRecordPermission:^(BOOL granted) {
+            if (granted) {
+                NSLog(@"Permission Granted");
+                
+                NSError *error;
+                
+                [session setCategory:@"AVAudioSessionCategoryPlayAndRecord" error:&error];
+            }
+            else {
+                NSLog(@"Permission Denied");
+            }
+        }];
+    }
+    else if ([buttonTitle isEqualToString:@"Motion Activity"]){
+        NSLog(@"Motion Acitivty");
+        
+        
+    }
+}
+
+- (ABAddressBookRef)addressBook {
+    return _addressBook;
+}
+
+- (void)setAddressBook:(ABAddressBookRef)newAddressBook {
+    if (_addressBook != newAddressBook) {
+        if (_addressBook != NULL) {
+            CFRelease(_addressBook);
+        }
+        if (newAddressBook != NULL) {
+            CFRetain(newAddressBook);
+        }
+        _addressBook = newAddressBook;
+    }
+}
+
+- (void)centralManagerDidUpdateState:(CBCentralManager *)central{
+    
 }
 
 @end
