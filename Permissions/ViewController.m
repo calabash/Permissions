fa8190982e81f0b97d00b51dd44444f926044aa7
Joshua Moody <joshuajmoody@gmail.com>
2015-08-26 13:54:16 +0200

    Suppress iOS 9 orientation method sig with #ifdef

--- Permissions/ViewController.m
+++ Permissions/ViewController.m
@@ -495,9 +495,15 @@ void handleAddressBookChange(ABAddressBookRef addressBook,
 
 #pragma mark - Orientation / Rotation
 
+#if __IPHONE_OS_VERSION_MAX_ALLOWED >= 90000
 - (UIInterfaceOrientationMask) supportedInterfaceOrientations {
   return UIInterfaceOrientationMaskAll;
 }
+#else
+- (NSUInteger) supportedInterfaceOrientations {
+  return UIInterfaceOrientationMaskAll;
+}
+#endif
 
 - (BOOL) shouldAutorotate {
   return YES;
