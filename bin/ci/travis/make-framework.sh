6ece268327afdd3085c289ce15a39cf9cbcc6616
Joshua Moody <joshuajmoody@gmail.com>
2015-11-06 14:29:12 +0100

    Travs: add CI scripts

---
+++ bin/ci/travis/make-framework.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+if [ -z "${TRAVIS}" ]; then
+  echo "FAIL: only run this script on Travis"
+  exit 1
+fi
+
+if [ "${TRAVIS_SECURE_ENV_VARS}" != "true" ]; then
+  echo "INFO: skipping keychain install; non-maintainer activity"
+  exit 0
+fi
+
+function error {
+  echo "$(tput setaf 1)ERROR: $1$(tput sgr0)"
+}
+
+function ditto_or_exit {
+  ditto "${1}" "${2}"
+  if [ "$?" != 0 ]; then
+    error "Could not copy:"
+    error "  source: ${1}"
+    error "  target: ${2}"
+    if [ ! -e "${1}" ]; then
+      error "The source file does not exist"
+      error "Did a previous xcodebuild step fail?"
+    fi
+    error "Exiting 1"
+    exit 1
+  fi
+}
+
+git clone \
+  --recursive \
+  --depth 1 \
+  --branch develop \
+  https://github.com/calabash/calabash-ios-server
+
+(cd calabash-ios-server && make framework)
+
+echo "path: ${PWD}"
+rm -rf calabash.framework
+ditto_or_exit calabash-ios-server/calabash.framework calabash.framework
+
+ls -al
+ls -al calabash.framework
+
