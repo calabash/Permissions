74cc6bfc9577abf61f9a023300322d2c46437d39
Joshua Moody <joshuajmoody@gmail.com>
2015-11-06 16:48:32 +0100

    Travis: gather more info about calabash.framework

--- bin/ci/travis/make-ipa.sh
+++ bin/ci/travis/make-ipa.sh
@@ -10,27 +10,32 @@ if [ "${TRAVIS_SECURE_ENV_VARS}" != "true" ]; then
   exit 0
 fi
 
+
+tree -h calabash.framework
+
+if [ -e calabash.framework/calabash ]; then
+  echo "Calabash framework binary does exist"
+else
+  echo "Calabash framework binary does not exist"
+  exit 1
+fi
+
+if [ -e /Users/travis/build/calabash/Permissions/calabash.framework/calabash ]; then
+  echo "Calabash framework exists at full path"
+else
+  echo "Calabash framework does not exist at full path"
+  exit 1
+fi
+
 bin/ci/travis/install-keychain.sh
 
 CODE_SIGN_DIR="${HOME}/.calabash/calabash-codesign/ios"
 KEYCHAIN="${CODE_SIGN_DIR}/ios/Calabash.keychain"
 
-# .env must exist or "make ipa" will not stage for submit
-DOTENV=".env"
-
-echo "IPA=Products/ipa/Permissions.ipa" > "${DOTENV}"
-echo "XTC_STAGING_DIR=xtc-submit" >> "${DOTENV}"
-echo "XTC_OTHER_GEMS_FILE=config/xtc-other-gems" >> "${DOTENV}"
-echo "XTC_CALABASH_GEM_DEV=1" >> "${DOTENV}"
-echo "XTC_RUN_LOOP_GEM_DEV=1" >> "${DOTENV}"
-echo "XTC_SERIES=master" >> "${DOTENV}"
-echo "XTC_DSYM=xtc-submit/Permissions.app.dSYM" >> "${DOTENV}"
-echo "XTC_WAIT_FOR_RESULTS=0" >> "${DOTENV}"
-echo "XTC_LOCALE=en_US" >> "${DOTENV}"
-echo "XTC_ACCOUNT=calabash-ios-ci" >> "${DOTENV}"
-echo "XTC_USER=joshua.moody@xamarin.com" >> "${DOTENV}"
+mv .env-template .env
 
 OUT=`xcrun security find-identity -p codesigning -v "${KEYCHAIN}"`
 IDENTITY=`echo $OUT | awk -F'"' '{print $2}' | tr -d '\n'`
-CODE_SIGN_IDENTITY="${IDENTITY}" make ipa
+echo "identity: ${IDENTITY}"
+XCPRETTY=0 CODE_SIGN_IDENTITY="${IDENTITY}" make ipa
 
