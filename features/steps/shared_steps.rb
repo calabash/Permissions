eb0737342b2b76b20cc212497e4ac5aadc26fc96
Joshua Moody <joshuajmoody@gmail.com>
2016-02-23 17:12:47 +0100

    Travis: wait longer for alerts to be dismissed

--- features/steps/shared_steps.rb
+++ features/steps/shared_steps.rb
@@ -79,7 +79,7 @@ Then(/^Calabash should dismiss the alert$/) do
   #
   # If the UIA call times out, then there is probably a privacy alert.
   if RunLoop::Environment.ci?
-    timeout = 15.0
+    timeout = 30.0
   elsif RunLoop::Environment.xtc?
     timeout = 8.0
   else
