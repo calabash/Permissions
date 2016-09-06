2c0893582ceecf6d365941757e675ac8f2215bc6
Joshua Moody <joshuajmoody@gmail.com>
2016-09-06 04:42:39 +0200

    Pry: try to improve pry + readline interactions

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 gem 'calabash-cucumber', :github => "calabash/calabash-ios", :branch => "develop"
 gem "run_loop", :github => "calabash/run_loop", :branch => "develop"
 gem "xamarin-test-cloud", "2.0.1"
+gem "rb-readline"
 
 # Sync these with config/xtc-other-gems
 gem "cucumber", "~> 2.0"
--- features/support/env.rb
+++ features/support/env.rb
@@ -1,5 +1,18 @@
 require 'run_loop'
 
+# Pry is not allowed on the Xamarin Test Cloud.  This will force a validation
+# error if you mistakenly submit a binding.pry to the Test Cloud.
+if !ENV['XAMARIN_TEST_CLOUD']
+  require 'pry'
+  Pry.config.history.file = '.pry-history'
+  require 'pry-nav'
+
+  require 'pry/config'
+  class Pry
+    trap('INT') { exit!(1) }
+  end
+end
+
 calabash = ENV['CALABASH']
 
 case calabash
