5ca613e60e278358d5a73e4ff24d756114d00c4d
Joshua Moody <joshuajmoody@gmail.com>
2017-03-22 14:06:25 +0100

    Jenkins: force Xcode 8.2.1

--- bin/ci/jenkins/run.sh
+++ bin/ci/jenkins/run.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+set +e
+
+export DEVELOPER_DIR=/Xcode/8.2.1/Xcode.app/Contents/Developer
+
+# Load correct CoreSimulatorService
+xcrun simctl help >/dev/null 2>&1
+xcrun simctl help >/dev/null 2>&1
+xcrun simctl help >/dev/null 2>&1
+
 set -e
 
 bundle update
