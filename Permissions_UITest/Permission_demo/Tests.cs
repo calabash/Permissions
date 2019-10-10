b653a1f253cc621f47e665a8cc7ddcccaf7dd268
Ahana <ahona.boul@gmail.com>
2019-10-10 16:17:35 -0700

    modified the tool versions and the test suite
    - Updated UITest and NUnit to the latest versions.
    
    - Modified the Test Suite so that each dialog is part of a separate [Test] instead of the same [Test]. The reason being because if say, Background Location fails but Push Notifications would pass;  it wouldn't be able to tell in the current set up because the test would fail before it gets the chance to test Push notifications.

--- Permissions_UITest/Permission_demo/Tests.cs
+++ Permissions_UITest/Permission_demo/Tests.cs
@@ -7,8 +7,6 @@ using Xamarin.UITest;
 using Xamarin.UITest.iOS;
 using Xamarin.UITest.Queries;
 
-
-
 namespace Permission_demo
 {
     [TestFixture]
@@ -32,142 +30,89 @@ namespace Permission_demo
             //    #endif
             app = ConfigureApp
                 .iOS
-              // TODO: Update this path to point to your iOS app and uncomment the
-              // code if the app is not included in the solution.
-              .AppBundle("/Users/ahanag22/Desktop/Permissions/Products/app/Permissions.app")
+                // TODO: Update this path to point to your iOS app and uncomment the
+                // code if the app is not included in the solution.
+                .AppBundle("/Users/ahanag22/Desktop/Permissions/Products/app/Permissions.app")
               .StartApp();
-
         }
 
-        public bool IsAlertVisible()
-        {
 
-            return app.Query(c => c.ClassFull("_UIAlertControllerView")).Any();
 
-        }
 
-        public void helperMethod(String s)
-        {
-            //loop until timeout(20 secs?) :
-            //tap the element
-            //wait for alert(1 sec?)
-            //if alert found - exit loop
-            //sleep 1 sec
+        //Adding Xamarin.UITest tests to the project https://github.com/calabash/Permissions
+        // App displays iOS privacy settings menu items// tapping each privacy settings menu items that pops up an alert
+        // checking if the popup is being handled by the logic
+        // the logic for dismissing popups does recognize the text of the popups 
+        // checked only the popups we've added to our logic here https://github.com/calabash/DeviceAgent.iOS/blob/develop/Server/Utilities/SpringBoardAlerts.m
 
-            bool IsVisible = false;
-            long startTime = Environment.TickCount;
-            while ((Environment.TickCount - startTime)< 20000)
-            {
-                app.Tap(x => x.Marked(s));
-                Thread.Sleep(1000);
-                if (IsAlertVisible())
-                {
 
-                    app.Screenshot("Alert Appeared");
-                    IsVisible = true;
-                    break;
-                }
-
-                Thread.Sleep(1000);
-
-            }
-            if (!IsVisible)
-            {
-                app.Screenshot("Alert has not been appeared");
-            }
-            if (IsVisible)
-            {
-                Thread.Sleep(5 * 1000);
-                if (!IsAlertVisible())
-                {
-                    app.Screenshot("Alert has been disappeared");
-                }
-                else
-                {
-                    app.Screenshot("Alert has not been disappeared");
-                }
-            }
-        }
-
-        // adding Xamarin.UITest tests to the project https://github.com/calabash/Permissions
-        // The test automates the tapping on the privacy settings menu
-        // checking if the the popups have been dismissed by the logic here https://github.com/calabash/DeviceAgent.iOS/blob/develop/Server/Utilities/SpringBoardAlerts.m
-
-        [Test]
-        public void AlertShowsUpForBackgroundLocation()
-        {
-            String s = "background location";
-            helperMethod(s);
-        }
 
         [Test]
         public void AlertShowsUpForContacts()
         {
-            String s = "contacts";
-            helperMethod(s);
-
+            // tapping menu item
+            app.Tap(x => x.Marked("contacts"));
+            //taking a screenshot
+            app.Screenshot("Show Alert for contacts");
+            app.DismissSpringboardAlerts();
+            Thread.Sleep(5 * 1000);
+            //chekcing if the alert has been dismissed
+            app.Screenshot("Alert dismissed");
         }
 
-         [Test]
-         public void AlertShowsUpForCalender()
-         {
-             String s = "calendar";
-             helperMethod(s);
-         }
+
 
         [Test]
-        public void AlertShowsUpForReminders()
+        public void AlertShowsUpForReminder()
         {
-            String s = "reminders";
-            helperMethod(s);
+            // tapping menu item
+            app.Tap(x => x.Marked("reminders"));
+            //taking a screenshot
+            app.Screenshot("Show Alert for reminders");
+            app.DismissSpringboardAlerts();
+            Thread.Sleep(5 * 1000);
+            //chekcing if the alert has been dismissed
+            app.Screenshot("Alert dismissed");
         }
 
         [Test]
         public void AlertShowsUpForMicrophone()
         {
-            String s = "microphone";
-            helperMethod(s);
+            // tapping menu item
+            app.Tap(x => x.Marked("microphone"));
+            //taking a screenshot
+            app.Screenshot("Show Alert for microphone");
+            app.DismissSpringboardAlerts();
+            Thread.Sleep(5 * 1000);
+            //chekcing if the alert has been dismissed
+            app.Screenshot("Alert dismissed");
         }
 
         [Test]
         public void AlertShowsUpForMotion()
         {
-            String s = "motion";
-            helperMethod(s);
-        }
-
-        [Test]
-        public void AlertShowsUpForCamera()
-        {
-            app.ScrollDown();
-            String s = "camera";
-            helperMethod(s);
-        }
-
-        //[Test]
-        //public void AlertShowsUpForBluetooth()
-        //{
-        //    app.ScrollDown();
-        //    app.Tap(x => x.Marked("bluetooth"));
-        //    app.Screenshot("After tapping the menu item blutooth");
-
-        //}
-
-
-        [Test]
-        public void AlertShowsUpForTwitter()
-        {
-            app.ScrollDown();
-            String s = "twitter";
-            helperMethod(s);
+            // tapping menu item
+            app.Tap(x => x.Marked("motion"));
+            //taking a screenshot
+            app.Screenshot("Show Alert for motion");
+            app.DismissSpringboardAlerts();
+            Thread.Sleep(5 * 1000);
+            //chekcing if the alert has been dismissed
+            app.Screenshot("Alert dismissed");
         }
 
         [Test]
         public void AlertShowsUpForAPNS()
         {
             app.ScrollDown();
-            String s = "apns";
-            helperMethod(s);
+            // tapping menu item
+            app.Tap(x => x.Marked("apns"));
+            //taking a screenshot
+            app.Screenshot("Show Alert for apns");
+            app.DismissSpringboardAlerts();
+            Thread.Sleep(5 * 1000);
+            //chekcing if the alert has been dismissed
+            app.Screenshot("Alert dismissed");
         }
 
 
@@ -175,40 +120,46 @@ namespace Permission_demo
         public void AlertShowsUpForAppleMusic()
         {
             app.ScrollDown();
-            String s = "apple music";
-            helperMethod(s);
+            // tapping menu item
+            app.Tap(x => x.Marked("apple music"));
+            //taking a screenshot
+            app.Screenshot("Show Alert for apple music");
+            app.DismissSpringboardAlerts();
+            Thread.Sleep(5 * 1000);
+            //chekcing if the alert has been dismissed
+            app.Screenshot("Alert dismissed");
         }
+
 
         [Test]
         public void AlertShowsUpForSpeechRecognition()
         {
             app.ScrollDown();
-            String s = "speech recognition";
-            helperMethod(s);
+            // tapping menu item
+            app.Tap(x => x.Marked("speech recognition"));
+            //taking a screenshot
+            app.Screenshot("Show Alert for speech recognition");
+            app.DismissSpringboardAlerts();
+            Thread.Sleep(5 * 1000);
+            //chekcing if the alert has been dismissed
+            app.Screenshot("Alert dismissed");
         }
 
+
         [Test]
-        public void AlertShowsUpForHealthKit()
+        public void AlertShowsUpForTwitter()
         {
             app.ScrollDown();
-            String s = "health kit";
-            helperMethod(s);
-            //app.Repl();
-
+            // tapping menu item
+            app.Tap(x => x.Marked("twitter"));
+            //taking a screenshot
+            app.Screenshot("Show Alert for twitter");
+            app.DismissSpringboardAlerts();
+            Thread.Sleep(5 * 1000);
+            //chekcing if the alert has been dismissed
+            app.Screenshot("Alert dismissed");
         }
-
-        [Test]
-        public void AlertShowsUpForPhotos()
-        {
-
-            app.Tap(x => x.Marked("photos"));
-            app.Screenshot("After tapping menu item Photos");
-            //app.Repl();
-            app.Tap(c => c.Class("UILabel").Text("Cancel"));
-
-        }
-
-
     }
 
 }
+
