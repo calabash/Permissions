985580aab1d457e91a4d288e47af0972dff4ea21
Dmitry Shibanov <v-dmshib@microsoft.com>
2019-10-30 12:43:49 +0300

    check common variant

--- bin/ci/appcenter.rb
+++ bin/ci/appcenter.rb
@@ -9,22 +9,16 @@ def main
   languages = ["da_DK","ru_RU","en_US","ja_JP"]
   threads = []
   languages.each do |lang|
-    threads << Thread.new(lang) do |item|
-      cmd = 'appcenter test run calabash' \
-      ' --app-path testcloud-submit/Permissions.ipa' \
-      ' --app App-Center-Test-Cloud/Permissions' \
-      ' --project-dir testcloud-submit' \
-      " --token #{AC_TOKEN}" \
-      " --test-series master" \
-      ' --devices "App-Center-Test-Cloud/daily-ios"' \
-      " --locale #{item}" \
-      ' --config-path cucumber.yml' \
-      ' --profile default' \
-      ' --disable-telemetry'
-      stdout, stderr, status = Open3.capture3(cmd)
-      puts "exit code language #{item} #{stdout}"
-      puts "status #{status}"
-      puts "stderr #{stderr}"    end
+    threads << Thread.new(lang) do |item| 
+      cmd = `appcenter test run calabash --app-path testcloud-submit/Permissions.ipa --app App-Center-Test-Cloud/Permissions --project-dir testcloud-submit --token #{AC_TOKEN} --test-series master --devices "App-Center-Test-Cloud/daily-ios" --locale #{item} --config-path cucumber.yml --profile default --disable-telemetry`
+      # ' > $TEMP/#{lang}.out'
+
+      # stdout, stderr, status = Open3.capture3(cmd)
+      # puts "exit code language #{item} #{stdout}"
+      # puts "status #{status}"
+      # puts "stderr #{stderr}"    
+      puts cmd
+    end
   end
   threads.each { |thr| thr.join }
 end
