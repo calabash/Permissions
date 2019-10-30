268cca5077a7997bcc5137d18832787cc0c6d45d
Dmitry Shibanov <v-dmshib@microsoft.com>
2019-10-30 10:41:16 +0300

    trying puts output

--- bin/ci/appcenter.rb
+++ bin/ci/appcenter.rb
@@ -4,27 +4,39 @@ require "pry"
 
 AC_TOKEN= `#{Dir.home}/.calabash/find-keychain-credential.sh api-token`.chomp
 
-def languages
-  languages = ["da_DK"]#,"ru_RU","en_US","ja_JP"]
+def main
+  languages = ["da_DK","ru_RU","en_US","ja_JP"]
   threads = []
-  languages.each do |item|
-    threads << Thread.new(item) do |i|
-      str = 'appcenter test run calabash' \
-      ' --app-path testcloud-submit/Permissions.ipa' \
-      ' --app App-Center-Test-Cloud/Permissions' \
-      ' --project-dir testcloud-submit' \
-      " --token #{AC_TOKEN}" \
-      " --test-series master" \
-      ' --devices "App-Center-Test-Cloud/daily-ios"' \
-      " --locale #{i}" \
-      ' --config-path cucumber.yml' \
-      ' --profile default' \
-      ' --disable-telemetry'
-      exit_code = Luffa.unix_command(str)
-      puts "exit code language #{i} is #{exit_code}"
+  languages.each do |lang|
+    threads << Thread.new(lang) do |item|
+      # cmd = 'appcenter test run calabash' \
+      # ' --app-path testcloud-submit/Permissions.ipa' \
+      # ' --app App-Center-Test-Cloud/Permissions' \
+      # ' --project-dir testcloud-submit' \
+      # " --token #{AC_TOKEN}" \
+      # " --test-series master" \
+      # ' --devices "App-Center-Test-Cloud/daily-ios"' \
+      # " --locale #{item}" \
+      # ' --config-path cucumber.yml' \
+      # ' --profile default' \
+      # ' --disable-telemetry'
+      # exit_code = Luffa.unix_command(cmd)
+      cmd = `appcenter test run calabash
+       --app-path testcloud-submit/Permissions.ipa
+       --app App-Center-Test-Cloud/Permissions
+       --project-dir testcloud-submit
+       --token #{AC_TOKEN}
+       --test-series master
+       --devices "App-Center-Test-Cloud/daily-ios"
+       --locale #{item}
+       --config-path cucumber.yml
+       --profile default
+       --disable-telemetry`
+      puts "exit code language #{item}"
+      puts cmd
     end
   end
   threads.each { |thr| thr.join }
 end
 
-languages
+main
