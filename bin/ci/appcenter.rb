9dac7da97c4f672ddf563cd6e0446a9156ff1fdc
Maxim Lobanov <v-malob@microsoft.com>
2019-10-31 13:08:51 +0300

    minor changes

--- bin/ci/appcenter.rb
+++ bin/ci/appcenter.rb
@@ -1,37 +1,28 @@
 #!/usr/bin/env ruby
 require "luffa"
-require "pry"
-require 'open3'
-require "parallel"
 
-AC_TOKEN= `#{Dir.home}/.calabash/find-keychain-credential.sh api-token`.chomp
+AC_TOKEN = `#{Dir.home}/.calabash/find-keychain-credential.sh api-token`.chomp
 
-def main
-  semaphore = Mutex.new
-  languages = ["da_DK","ru_RU","en_US","ja_JP"]
-  threads = []
-  summary = "Summary\n"
-  languages.each do |lang|
-    threads << Thread.new(lang) do |item|
-      output = `appcenter test run calabash --app-path testcloud-submit/Permissions.ipa --app App-Center-Test-Cloud/Permissions --project-dir testcloud-submit --token #{AC_TOKEN} --test-series master --devices "App-Center-Test-Cloud/v-malob-test-permissions" --locale #{item} --config-path cucumber.yml --profile default --disable-telemetry`
-      semaphore.synchronize {
-        puts "Run tests for language '#{item}'"
-        puts output
-        puts "Finished with exit code '#{$?.exitstatus}'"
-        summary+="Language '#{item}' finished with exit code '#{$?.exitstatus}'\n"
-      }
+semaphore = Mutex.new
+languages = ["da_DK", "ru_RU", "en_US", "ja_JP"]
+threads = []
+summary = {}
+languages.each do |lang|
+  threads << Thread.new(lang) do |item|
+    output = `appcenter test run calabash --app-path testcloud-submit/Permissions.ipa --app App-Center-Test-Cloud/Permissions --project-dir testcloud-submit --token #{AC_TOKEN} --test-series master --devices "App-Center-Test-Cloud/v-malob-test-permissions" --locale #{item} --config-path cucumber.yml --profile default --disable-telemetry`
+    semaphore.synchronize do
+      puts "Run tests for language '#{item}'"
+      puts output
+      puts "Finished with exit code '#{$?.exitstatus}'"
+      summary[item] = $?.exitstatus
     end
   end
-  # results = Parallel.map(languages, in_threads: 4) do |item|
-  #   cmd = `appcenter test run calabash --app-path testcloud-submit/Permissions.ipa --app App-Center-Test-Cloud/Permissions --project-dir testcloud-submit --token #{AC_TOKEN} --test-series master --devices "App-Center-Test-Cloud/daily-ios" --locale #{item} --config-path cucumber.yml --profile default --disable-telemetry`
-  #   semaphore.synchronize {
-  #     puts "Testing language is #{item}"
-  #     puts item
-  #     puts cmd
-  #   }
-  # end
-  threads.each { |thr| thr.join }
-  puts summary
 end
 
-main
+threads.each { |thr| thr.join }
+
+puts "Summary:"
+JSON.pretty_generate(summary)
+
+# exit 0 if all tests passed
+exit summary.values.sum
